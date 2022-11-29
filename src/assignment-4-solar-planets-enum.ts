c1dd4cf7183803910105d2977a830c0e7b8a6064
azhan.khan@codecraft.co.in <azhan.khan@codecraft.co.in>
2022-11-29 16:00:39 +0530

    assignment 4 implementation

---
+++ src/assignment-4-solar-planets-enum.ts
@@ -0,0 +1,15 @@
+// Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune enum
+enum PlanetsEnum {
+  Mercury = 'mercury',
+  Venus = 'venus',
+  Earth = 'earth',
+  Mars = 'mars',
+  Jupiter = 'jupiter',
+  Saturn = 'saturn',
+  Uranus = 'uranus',
+  Neptune = 'neptune',
+}
+
+console.log(PlanetsEnum.Jupiter);
+console.log(PlanetsEnum.Earth);
+console.log(PlanetsEnum.Neptune);
