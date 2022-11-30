0a6a5297792f01ccf823fac88d0298ab0f38456a
azhan.khan@codecraft.co.in <azhan.khan@codecraft.co.in>
2022-11-30 09:28:55 +0530

    functional programming assignment-3 test file docs changed'

--- src/functional-programming/fp-assignment-3-filter-out-all-lines.test.ts
+++ src/functional-programming/fp-assignment-3-filter-out-all-lines.test.ts
@@ -1,7 +1,7 @@
 import {filterOutNot4} from './fp-assignment-3-filter-out-all-lines';
 
-describe('filterout all lines not containing 4 test', () => {
-  test('check for filter test', () => {
+describe('check for filter  test', () => {
+  test(' filterout all lines not containing 4', () => {
     expect(filterOutNot4()).toEqual(['items qty', 'mango 60', 'onion 41', 'water 20']);
   });
 });
